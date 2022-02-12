9244a49a5520796470f9e6703dbf9c3f07d46584
kbrew01 <60008912+kbrew01@users.noreply.github.com>
2022-02-12 10:27:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,26 +8,26 @@ def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
-  #Fill in start
+  serverSocket.listen(5)
 
-  #Fill in end
+  print('the web server is up on port:',port)
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read()
         
         #Send one HTTP header line into socket.
-        #Fill in start
+        connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
-        #Fill in end
+
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -36,16 +36,17 @@ def webServer(port=13331):
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
-        # Send response message for file not found (404)
-        #Fill in start
+        #Send response message for file not found (404)
+        connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        connectionSocket.send(b"<html><head></head><body>404 not found</body></html>")
 
-        #Fill in end
+
 
 
         #Close client socket
-        #Fill in start
+        connectionSocket.close()
 
-        #Fill in end
+
 
     except (ConnectionResetError, BrokenPipeError):
       pass
